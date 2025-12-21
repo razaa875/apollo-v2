737c858d7d01bc17785388f983c1fd1cf53c7767
Hassan <hassanot234@gmail.com>
2025-12-21 21:24:38 +0500

    apollo thinks

--- src/app/(routes)/products/[slug]/page.tsx
+++ src/app/(routes)/products/[slug]/page.tsx
@@ -6,12 +6,11 @@ import { apiService } from "@/common/services";
 import { IProduct } from "@/common/models/interface";
 import Hero from "./_components/hero";
 import ProductReview from "./_components/review";
-import RecentTest from "../../(home)/_components/recent-test";
 import LifeExpectency from "./_components/life-expectency";
-import Comparison from "./_components/comparison";
-import WhyApollo from "../../(home)/_components/why-apollo";
-import { Separator } from "@/components/ui/separator";
 import JournalGuide from "./_components/journal-guide";
+import WhatsThink from "./_components/whats-think";
+import FeelFree from "./_components/feel-free";
+import ApolloIndex from "./_components/apollo-index";
 
 export default function ProductDetailPage() {
   const params = useParams(); // Get slug from route
@@ -58,15 +57,13 @@ export default function ProductDetailPage() {
   return (
     <main className="pt-28 md:pt-32">
       <Hero product={product} />
-      <RecentTest />
+      <WhatsThink />
       <ProductReview product={product} />
       <LifeExpectency product={product} />
-      <Comparison product={product} />
-      <div>
-        <Separator className="mb-8 md:mb-12 mt-4 md:mt-8" />
-        <WhyApollo />
-      </div>
-      <JournalGuide product={product}/>
+      <ApolloIndex product={product}/>
+      {/* <Comparison product={product} /> */}
+      <FeelFree />
+      <JournalGuide product={product} />
     </main>
   );
 }
